7af5dd51e9e0a1bdae2b2a0c2272c7897c5d8846
iFlow CLI <iflow@example.com>
2025-09-30 15:34:25 +0800

    修复Frpclib.framework头文件引用问题，使用相对路径引用

--- Natives/FrpcBridge.m
+++ Natives/FrpcBridge.m
@@ -1,6 +1,6 @@
 #import "FrpcBridge.h"
 #import "utils.h"
-#import <Frpclib/Frpclib.h>
+#import "Frpclib.framework/Versions/A/Headers/Frpclib.h"
 
 @interface FrpcBridge ()
 @property (nonatomic, assign) BOOL isRunning;
